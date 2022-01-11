0d0403ca88322f6324a298123c743bf7ef455433
hoangvictor <41613226+hoangvictor@users.noreply.github.com>
2022-01-11 18:35:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,4 @@ var app = express();
 
 app.use(express.static(__dirname));
 
-// app.listen(process.env.PORT || 3000);
-port = 3000
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000);
